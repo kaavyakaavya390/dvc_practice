7099d8008ed00d16e8eb681fee5b196c319740fd
kaavyakaavya390 <kaavyakaavya390@gmail.com>
2025-09-18 22:12:25 +0530

    bug check

--- src/split.py
+++ src/split.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 def main(input_file, train_file, test_file):
     # Read the raw data
     df = pd.read_csv(input_file)
-
+    #print(df)
     # Simple train/test split (80/20)
     train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
